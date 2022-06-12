9d703ebefeb9cd47d1c0edd18b395c6912ec268e
Jason Allen <39307371+jallend1@users.noreply.github.com>
2022-06-11 17:12:04 -0700

    Reveals fact source in header on click

--- src/Components/FactCard.jsx
+++ src/Components/FactCard.jsx
@@ -1,17 +1,32 @@
+import { useState } from 'react';
 import { format } from 'date-fns';
 
 const FactCard = ({ title, tags, date, source, icon, id }) => {
-  console.log(icon);
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const flipCard = () => {
+    setIsFlipped(!isFlipped);
+  };
+
   return (
     <div className="fact-card">
       <div className="fact-card__header">
-        <p>{format(date, 'MMM do, yyyy')}</p>
-        <img
-          src={require('../assets/images/icons/' + icon)}
-          className="fact-card-icon"
-          alt={icon.split('.')[0] + ' icon'}
-        />
-        <p>{source}</p>
+        {!isFlipped ? (
+          <>
+            <p>{format(date, 'MMM do, yyyy')}</p>
+            <img
+              src={require('../assets/images/icons/' + icon)}
+              className="fact-card-icon"
+              alt={icon.split('.')[0] + ' icon'}
+            />
+            <p onClick={flipCard}>See Source</p>
+          </>
+        ) : (
+          <div className="fact-card-source-header">
+            <p>{source}</p>
+            <p onClick={flipCard}>Back</p>
+          </div>
+        )}
       </div>
       <div className="fact-card__body">
         <h2 className="fact-card__title">{title}</h2>
